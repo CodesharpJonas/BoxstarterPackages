edb5a54c25b7706b86e5c5ff540a27265385eb6b
Jonas Versen <jonas.versen@codesharp.se>
2019-10-23 20:01:15 +0200

    Downgrade to sql express sp1

--- sqllocaldb/tools/chocolateyinstall.ps1
+++ sqllocaldb/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.microsoft.com/download/4/1/A/41AD6EDE-9794-44E3-B3D5-A1AF62CD7A6F/sql16_sp2_dlc/en-us/SqlLocalDB.msi'
+$url        = 'https://download.microsoft.com/download/9/0/7/907AD35F-9F9C-43A5-9789-52470555DB90/ENU/SqlLocalDB.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -11,7 +11,7 @@ $packageArgs = @{
 
   softwareName  = 'sqllocaldb*'
 
-  checksum      = '7152f34edbedf177d05136074939f7188bb49147de0ebbd829d1a5c85c53d6d4'
+  checksum      = '413e739b79068ba0d91a8c62db14bccd37591e86515f4ae74f4c91c270f02d45'
   checksumType  = 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" IACCEPTSQLLOCALDBLICENSETERMS=YES"
